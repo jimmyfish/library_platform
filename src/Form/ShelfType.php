50f4b3774da940bef1a2106448cb31115b98d451
Dito YP <killcoder212@gmail.com>
2019-10-15 17:31:01 +0700

    add form type for shelf

---
+++ src/Form/ShelfType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Shelf;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ShelfType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('shelfIdentifier')
+            ->add('floor')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Shelf::class,
+        ]);
+    }
+}
